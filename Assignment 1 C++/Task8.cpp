8a30c224023a0210a5c2af9719fe659aab3b46f2
Sebemblad <Sebemblad@gmail.com>
2022-12-04 00:42:40 +0100

    task 8 start

--- Assignment 1 C++/Task8.cpp
+++ Assignment 1 C++/Task8.cpp
@@ -1,11 +1,26 @@
-/* pyramid
+/* pyramid ASCII uppercase letter 65-90 A-Z
+* input 3
 * 1
 * 12
 * 123
+* input C
 * A
-* AA
-* AAA
+* BB
+* CCC
 */
-
 #include <iostream>
+#include <stdio.h>
 using namespace std;
+
+int main()
+{
+	char inp;
+
+	printf("Enter number or uppercase letter: ");
+	cin >> inp;
+	cout << inp << endl;
+
+	
+
+
+}
