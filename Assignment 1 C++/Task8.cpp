d9ca21b59d6d586feb72ab97ae6259810939b6c8
Sebemblad <Sebemblad@gmail.com>
2022-12-04 18:35:03 +0100

    task 8

--- Assignment 1 C++/Task8.cpp
+++ Assignment 1 C++/Task8.cpp
@@ -1,25 +1,44 @@
-/* pyramid ASCII uppercase letter 65-90 A-Z
-* input 3
-* 1
-* 12
-* 123
-* input C
-* A
-* BB
-* CCC
-*/
 #include <iostream>
 #include <stdio.h>
 using namespace std;
 
 int main()
 {
-	char inp;
+	char inp, alphabet = 'A';
 
 	printf("Enter number or uppercase letter: ");
 	cin >> inp;
-	cout << inp << endl;
 
+	if (int(inp) >= 65 && int(inp) <= 90) // check if input is an uppercase letter then perform pyramid
+	{
+		for (int i = 1; i <= (inp - 'A' + 1); ++i)
+		{
+			for (int j = 1; j <= i; ++j)
+			{
+				cout << alphabet << " ";
+			}
+			++alphabet;
+
+			cout << endl;
+		}
+	}
+
+	if (isdigit(inp) == 4) // check if input is number then perform pyramid (4 is the value for true)
+	{
+		for (int i = 1; i <= inp-48; ++i)
+		{
+			for (int j = 1; j <= i; ++j)
+			{
+				cout << j << " ";
+			}
+			printf("\n");
+		}
+	}
+
+	else
+	{
+		printf("output must be a number or uppercase letter");
+	}
 	
 
 
